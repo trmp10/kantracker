5eab2305c53d89453819951af3036b506384b6b4
toni <paduatoni@gmail.com>
2026-06-04 13:08:35 +0800

    Initial Commit

---
+++ KanTracker/KanTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanTrackerApp.swift
+//  KanTracker
+//
+//  Created by Toni Rose Mitre Padua on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct KanTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
